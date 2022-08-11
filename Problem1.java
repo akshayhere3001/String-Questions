62744a9bc90537e6121158e600d77894f3596d30
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-08-11 12:11:38 +0530

    Create Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,15 @@
+public class Problem1{
+  public static void main(String[] args){
+    
+    
+    // Problem - 1 -> Reverse a String
+		String greet = "hello";
+		char[] chArr = greet.toCharArray();
+		
+		for(int i = chArr.length - 1; i>=0; i--) {
+			System.out.print(chArr[i]);
+		}
+    
+    
+  }
+}
