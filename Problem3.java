3b219ec807442754f1a3dc1eaa1749f8baafc863
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-08-11 12:28:32 +0530

    Create Problem3.java

---
+++ Problem3.java
@@ -0,0 +1,14 @@
+public class Problem3{
+  public static void main(String[] args){
+  
+    
+      
+    
+		// Problem - 3 -> Remove White Spaces
+		String greet = "  he   l  l  o   ";
+		
+		greet = greet.replaceAll("\\s", "");
+		System.out.print(greet);
+  
+  }
+}
